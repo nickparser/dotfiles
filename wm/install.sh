7f097d7cc67b393d13f3a05b17c6fd8d6aec2024
Nikita <nick.parser@gmail.com>
2022-05-15 15:02:15 +0200

    resolve conflicts

--- wm/install.sh
+++ wm/install.sh
@@ -1,28 +1,13 @@
 #/usr/bin/bash
 
-# remove DE package 
-sudo apt -y autoremove --purge ubuntu-desktop
-sudo apt update
-
-
 # install dependencies
-sudo apt install xorg suckless-tools build-essential libx11-dev libxinerama-dev libxft-dev git vim dmenu xdm -y
+sudo pacman -Sy base-devel xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim
 
-# go to the home directory
-cd ~
-
-# clone the dotfiles repo
-git clone https://github.com/nickparser/dotfiles.git
-
-# go to the .dwm directory
-cd dotfiles/wm/.dwm
-
-# compile dwm
+# go to the dwm directory & compile dwm
+cd ~/dotfiles/wm/dwm
 sudo make clean install
-echo “exec dwm” > ~/.xsession
+echo “exec dwm” >> ~/.xinitrc
+echo "startx" >> ~/.bash_profile
 
 # copy dwm dir to the home dir
-cp -r dotfiles/wm/.dwm .
-
-sudo chown nickparser:nickparser .Xauthority
-reboot
+cp -r dotfiles/wm/dwm .
