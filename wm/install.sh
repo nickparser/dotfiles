e95a3abfe13726ffb103b580493afbd3eaada619
Nikita <nick.parser@gmail.com>
2022-05-16 13:33:31 +0200

    autostart.sh added

--- wm/install.sh
+++ wm/install.sh
@@ -1,7 +1,7 @@
 #/usr/bin/bash
 
 # install dependencies
-sudo pacman -Sy base-devel xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim
+sudo pacman -Sy base-devel xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim dmenu 
 
 # go to the dwm directory & compile dwm
 cd ~/dotfiles/wm/dwm
@@ -12,3 +12,4 @@ echo "startx" >> ~/.bash_profile
 # copy dwm dir to the home config dir
 mkdir -p ~/.config
 cp -r ~/dotfiles/wm/dwm ~/.config
+touch ~/.config/autostart.sh
