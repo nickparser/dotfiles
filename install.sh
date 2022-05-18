c237c1cc2675556d738d06446a50ec3479d6f537
Nikita <nick.parser@gmail.com>
2022-05-18 20:42:57 +0200

    slock is removed

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/usr/an/bash
 
 # install dependencies
-sudo pacman -Sy xorg-server xorg-xsetroot xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim dmenu 
+sudo pacman -Sy xorg-server xorg-xsetroot xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim dmenu slock
 
 # install paru
 sudo pacman -S --nedded base-devel
@@ -19,13 +19,8 @@ sudo make clean install
 echo "exec dwm" >> ~/.xinitrc
 echo "startx" >> ~/.bash_profile
 
-# compile slock
-cd ~/dotfiles/slock
-sudo make clean install
-
 # create config dir
 mkdir -p ~/.config
 cp -r ~/dotfiles/dwm ~/.config
-cp -r ~/dotfiles/slock ~/.config
 cp ~/dotfiles/autostart.sh ~/.config/autostart.sh
 chmod +x ~/.config/autostart.sh
