9f4f351ea4941953affe35326bbb9c4a76a1ee03
Nikita <nick.parser@gmail.com>
2022-05-28 19:04:59 +0200

    zsh added

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/usr/an/bash
 
 # install dependencies
-sudo pacman -Sy xorg-server xorg-xsetroot xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty vim dmenu slock
+sudo pacman -Sy xorg-server xorg-xsetroot xorg-xinit libx11 libxinerama libxft webkit2gtk alacritty gvim wget dmenu slock
 
 # install paru
 sudo pacman -S --nedded base-devel
@@ -10,13 +10,19 @@ cd paru
 makepkg -si
 
 # install utilities
-paru -S arandr ttf-font-awesome ttf-anonymous-pro feh picom
+paru -S arandr ttf-font-awesome ttf-anonymous-pro feh picom zsh
+
+# install oh-my-zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+cp ~/dotfiles/.zshrc ~/
 
 # compile dwm
 cd ~/dotfiles/dwm
 sudo make clean install
 echo "exec dwm" >> ~/.xinitrc
-echo "startx" >> ~/.bash_profile
+echo "startx" >> ~/.zprofile
 
 # create config dir
 mkdir -p ~/.config
@@ -26,3 +32,4 @@ cp ~/dotfiles/wallpaper.png ~/.config
 cp -r ~/dotfiles/alacritty ~/.config
 
 chmod +x ~/.config/autostart.sh
+
